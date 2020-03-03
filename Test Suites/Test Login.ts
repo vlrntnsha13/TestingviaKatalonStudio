920c3eb77bfbbbaa4f482379a944518db5414b90
vlrntnsha13 <valeriana.tanesha@si.ukdw.ac.id>
2020-03-03 18:40:03 +0700

    initial commit

---
+++ Test Suites/Test Login.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Menguji login dengan data yang benar dan salah</description>
+   <name>Test Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a193872e-c5e9-4bce-a071-5f2e7715c385</testSuiteGuid>
+   <testCaseLink>
+      <guid>a03bbb54-91db-45c1-88f6-1021634e4afa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Tes Login Dian dan Kopi</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>83a7e8f5-6a7b-4386-8db0-3aca2bee99ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Tes Login Dians dan Kopi</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
